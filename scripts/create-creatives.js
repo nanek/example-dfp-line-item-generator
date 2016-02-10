d44b85be3c24a87a172860959b0ab887b3c3a641
Juan Caicedo <juan@spanishdict.com>
2016-02-10 11:02:41 -0500

    style fixes in create-creatives

--- scripts/create-creatives.js
+++ scripts/create-creatives.js
@@ -13,14 +13,13 @@
 'use strict';
 
 var Bluebird = require('bluebird');
-var formatter = require('../lib/formatter');
 var _ = require('lodash');
 var ProgressBar = require('progress');
 var progressBar;
 var argv = require('minimist')(process.argv.slice(2));
 
 var DFP_CREDS = require('../local/application-creds');
-var config = require('../local/config')
+var config = require('../local/config');
 var formatter = require('../lib/formatter');
 
 var Dfp = require('node-google-dfp-wrapper');
@@ -29,22 +28,22 @@ var credentials = {
   clientId: DFP_CREDS.installed.client_id,
   clientSecret: DFP_CREDS.installed.client_secret,
   redirectUrl: DFP_CREDS.installed.redirect_uris[0]
-}
+};
 
 var dfp = new Dfp(credentials, config, config.refreshToken);
 
+// read command line arguments
 var channel = argv.channel;
 var region = argv.region;
 var position = argv.position;
 var partner = argv.partner;
 var platform = argv.platform;
 
+// use arguments to determine any other variables
 var pricePoints = require('./price-points');
 var sizes = require('./sizes')(platform);
-
 var size = sizes[position];
 var slots = require('../input/index-slot')(platform);
-
 var slot = slots[position];
 
 var CONCURRENCY = {
@@ -78,7 +77,7 @@ function getCombinations() {
       region: region,
       channel: channel,
       position: position,
-      number: number,
+      number: number
     });
 
     combinations.push(creative);
@@ -110,7 +109,7 @@ function logSuccess(results) {
 }
 
 function handleError(err) {
-  progressBar.tick()
+  progressBar.tick();
   console.log('creating creatives failed');
   console.log('because', err.stack);
 }
@@ -119,4 +118,4 @@ Bluebird.resolve(getCombinations())
   .map(prepareCreative, CONCURRENCY)
   .then(createCreatives)
   .then(logSuccess)
-  .catch(handleError)
+  .catch(handleError);
